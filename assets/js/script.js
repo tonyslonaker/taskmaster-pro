7a521925d95e9d690a12e33fbbd374665842670c
Tony Slonaker <tony.slonaker@gmail.com>
2021-08-18 10:06:29 -0600

    Edit Task Dates

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,12 +10,39 @@ var createTask = function(taskText, taskDate, taskList) {
     .addClass("m-1")
     .text(taskText);
 
+  // recreate p element
+  var taskP = $("<p>")
+    .addClass("m-1")
+    .text(text);
+
+  // replace textarea with p element
+  $(this).replaceWith(taskP);
+
   // append span and p element to parent li
   taskLi.append(taskSpan, taskP);
 
 
   // append to ul list on the page
   $("#list-" + taskList).append(taskLi);
+
+  $(".list-group").on("blur", "textarea", function() {
+
+  // get the textarea's current value/text
+  var text = $(this)
+  .val()
+  .trim();
+
+  // get the parent ul's id attribute
+  var status = $(this)
+  .closest(".list-group")
+  .attr("id")
+  .replace("list-", "");
+
+  // get the task's position in the list of other li elements
+  var index = $(this)
+  .closest(".list-group-item")
+  .index();
+  });
 };
 
 var loadTasks = function() {
@@ -28,6 +55,9 @@ var loadTasks = function() {
       inProgress: [],
       inReview: [],
       done: []
+      //tasks[status][index].text = text;
+      
+      //saveTasks();
     };
   }
 
@@ -43,9 +73,29 @@ var loadTasks = function() {
 
 var saveTasks = function() {
   localStorage.setItem("tasks", JSON.stringify(tasks));
+
+  $(".list-group").on("click", "p", function() {
+    var text = $(this)
+    .text()
+    .trim();
+    console.log(text);
+
+    var textInput = $("<textarea>")
+    .addClass("form-control")
+    .val(text);
+
+    $(this).replaceWith(textInput);
+
+    textInput.trigger("focus");
+  });
 };
 
+$(".list-group").on("blur", "textarea", function() {
 
+});
+
+// due date was clicked
+$(".list-group")
 
 
 // modal was triggered
