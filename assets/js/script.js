54cd8e8081452bd2ac3fd6df6b22652b8b5cb940
Tony Slonaker <tony.slonaker@gmail.com>
2021-08-18 10:34:10 -0600

    update bootstrap classes

--- assets/js/script.js
+++ assets/js/script.js
@@ -101,6 +101,38 @@ var date = $(this)
 .text()
 .trim();
 
+//value of due date was changed
+$(".list-group").on("blur", "input[type='text']", function() {
+  //get current text
+  var date = $(this)
+  .val()
+  .trim();
+
+  //get the parent ul's id attribute
+  var status = $(this)
+  .closest(".list-group")
+  .attr("id")
+  .replace("list-", "");
+
+  //get the task's position in the list of other li elements
+  var index = $(this)
+  .closest(".list-group-item")
+  .index();
+
+  //update task in array and re-save to localstorage
+  tasks[status][index].date = date;
+  saveTasks();
+
+  // recreate span element with bootstrap classes
+  var taskSpan = $("<span>")
+  .addClass("badge badge-primary badge-pill")
+  .text(date);
+
+  //replace input with span element
+  $(this).replaceWith(taskSpan);
+
+});
+
 //create new input element
 var dateInput = $("<input>")
 .attr("type", "text")
