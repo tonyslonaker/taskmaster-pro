63d6c9fa6b14413bb1acf383238015e121e107e7
Tony Slonaker <tony.slonaker@gmail.com>
2021-08-20 19:49:48 -0600

    update sortable

--- assets/js/script.js
+++ assets/js/script.js
@@ -173,5 +173,27 @@ $("#remove-tasks").on("click", function() {
   saveTasks();
 });
 
+$(".card .list-group").sortable({
+  connectWith: $(".card .list-group"),
+  scroll: false,
+  tolerance: "pointer",
+  helper: "clone",
+  activate: function(event) {
+    console.log("activate", this);
+  },
+  deactivate: function(event) {
+    console.log("deactivate", this);
+  },
+  over: function(event) {
+    console.log("over", event.target);
+  },
+  out: function(event) {
+    console.log("out", event.target);
+  },
+  update: function(event) {
+    console.log("update", this);
+  }
+});
+
 // load tasks for the first time
 loadTasks();
